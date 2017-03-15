4fb50521b114dd64c01e69012ad662665859384d
zlevi91 <zlevi91@gmail.com>
2017-03-15 17:28:28 +0200

    תרגיל 2

--- Exercise02_zofiya/Test/com/company/MenuTest.java
+++ Exercise02_zofiya/Test/com/company/MenuTest.java
@@ -1,10 +1,13 @@
 package com.company;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by hackeru on 3/15/2017.
@@ -12,26 +15,43 @@ import static org.junit.jupiter.api.Assertions.*;
 class MenuTest {
     @BeforeEach
     void setUp() {
+        System.out.println("Begin test Menu");
 
     }
 
     @AfterEach
     void tearDown() {
+        System.out.println("After test Menu");
 
     }
 
     @Test
     void printMenu() {
 
+
     }
 
     @Test
     void chooseOption() {
+        Menu myMenu = new Menu();
+        String testChoice = " ";
+        Assertions.assertEquals(myMenu.myChoice(testChoice), "invalid option. try again.", "assertion failed for space");
+        testChoice = "\n";
+        Assertions.assertEquals(myMenu.myChoice(testChoice), "invalid option. try again.", "assertion failed for Enter");
+        testChoice = "a";
+        Assertions.assertEquals(myMenu.myChoice(testChoice), "invalid option. try again.", "assertion failed for letter");
+        testChoice = "6";
+        Assertions.assertEquals(myMenu.myChoice(testChoice), "invalid option. try again.", "assertion failed for number except 1,2");
 
     }
 
     @Test
-    void myChoice() {
+    void chooseOption2() {
+        Menu myMenu = new Menu();
+        Input input = mock(Input.class);
+        when(input.input()).thenReturn("1").thenReturn("C:\\Users\\hackeru.HACKERU3\\Downloads\\z.txt").thenReturn("0");
+        
+        //Assertions.assertEquals(myMenu.myChoice("1"),,"gfi");
 
     }
 
