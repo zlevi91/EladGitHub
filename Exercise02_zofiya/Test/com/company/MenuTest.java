22dc37083c3e59764986daa7d460756e74886287
zlevi91 <zlevi91@gmail.com>
2017-03-16 11:33:59 +0200

    תרגיל 2

--- Exercise02_zofiya/Test/com/company/MenuTest.java
+++ Exercise02_zofiya/Test/com/company/MenuTest.java
@@ -1,59 +1,75 @@
-package com.company;
+        package com.company;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.mock;
+        import org.junit.jupiter.api.AfterEach;
+        import org.junit.jupiter.api.Assertions;
+        import org.junit.jupiter.api.BeforeEach;
+        import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+        import static org.junit.jupiter.api.Assertions.*;
+        import static org.mockito.Mockito.*;
 
 /**
- * Created by hackeru on 3/15/2017.
+ * Created by hackeru on 3/16/2017.
  */
 class MenuTest {
+
+    int outputCount = 0;
+
     @BeforeEach
     void setUp() {
         System.out.println("Begin test Menu");
-
     }
 
     @AfterEach
     void tearDown() {
         System.out.println("After test Menu");
-
     }
 
     @Test
     void printMenu() {
-
-
+// mock()
     }
 
     @Test
-    void chooseOption() {
+    void chooseOptionTest(){
+        Input input = mock(Input.class);
+        when(input.input()).thenReturn("1").thenReturn("C:\\Users\\hackeru.HACKERU3\\Documents\\GitHub\\Exercisess\\Exercises\\z.txt").thenReturn("0");
+        Output output = new Output() {
+            @Override
+            public void output(String s) {
+                switch (outputCount){
+                    case 0:
+                        assertEquals("please choose:\n 1. If you want to encrypt the file Press 1\n 2. If you want to decrypt the file Press 2\n 0. exit\n your choice:", s);
+                        break;
+                    case 1:
+                        assertEquals("Enter a file path:", s);
+                        break;
+                    case 2:
+                        assertEquals("encrypt", s);
+                        break;
+                }
+                outputCount++;
+            }
+        };
+        Menu myMenu = new Menu(output,input);
+        myMenu.printMenu();
+    }
+
+
+
+    @Test
+    void myChoice() {
         Menu myMenu = new Menu();
-        String testChoice = " ";
-        Assertions.assertEquals(myMenu.myChoice(testChoice), "invalid option. try again.", "assertion failed for space");
+        String testChoice = "";
+        Assertions.assertEquals(myMenu.myChoice(testChoice), "Something went wrong. Please try again", "assertion failed for blank");
         testChoice = "\n";
         Assertions.assertEquals(myMenu.myChoice(testChoice), "invalid option. try again.", "assertion failed for Enter");
         testChoice = "a";
         Assertions.assertEquals(myMenu.myChoice(testChoice), "invalid option. try again.", "assertion failed for letter");
         testChoice = "6";
         Assertions.assertEquals(myMenu.myChoice(testChoice), "invalid option. try again.", "assertion failed for number except 1,2");
-
     }
 
-    @Test
-    void chooseOption2() {
-        Menu myMenu = new Menu();
-        Input input = mock(Input.class);
-        when(input.input()).thenReturn("1").thenReturn("C:\\Users\\hackeru.HACKERU3\\Downloads\\z.txt").thenReturn("0");
-        
-        //Assertions.assertEquals(myMenu.myChoice("1"),,"gfi");
-
-    }
 
     @Test
     void getPathFromUser() {
--- Exercise02_zofiya/src/com/company/Menu.java
+++ Exercise02_zofiya/src/com/company/Menu.java
@@ -6,18 +6,28 @@ import java.io.File;
  * Created by hackeru on 3/15/2017.
  */
 public class Menu {
-    Output myOutput = new ScreenOutput();
-    Input myInput = new ScreenInput();
+    Output myOutput; //= new ScreenOutput();
+    Input myInput ;//= new ScreenInput();
     FileOperations myFileOperations = new FileOperations();
     EncryptFile encryptFile = new EncryptFile();
     DecryptFile decryptFile = new DecryptFile();
     String filePathString;
     File filePath;
 
+    public Menu(Output myOutput, Input myInput) {
+        this.myOutput = myOutput;
+        this.myInput = myInput;
+    }
+
+    public Menu(){
+        myOutput = new ScreenOutput();
+        myInput = new ScreenInput();
+
+    }
 
     public void printMenu() {
 
-        myOutput.output("please choose:\n 1. If you want to encrypt the file Press 1\n 2. If you want to decrypt the file Press 2\n 0. exit\n your choice:   ");
+        myOutput.output("please choose:\n 1. If you want to encrypt the file Press 1\n 2. If you want to decrypt the file Press 2\n 0. exit\n your choice:");
         chooseOption();
 
     }
